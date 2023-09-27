6bb661a456bafbf7be135e3b7c2f5b6b567fa243
Mehdi Raheemun <mraheemu@student.42.fr>
2023-09-27 15:59:06 +0200

    Ajout navBar statique

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+
 function App() {
   return (
     <div>
-      <header>
-        <p>Initial commit</p>
-      </header>
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../../img/argentBankLogo.png";
+import "./NavBar.css";
+
+function NavBar() {
+  return (
+    <nav className="main-nav">
+      <a className="main-nav-logo" href="./index.html">
+        <img
+          className="main-nav-logo-image"
+          src={logo}
+          alt="Argent Bank Logo"
+        />
+        <h1 className="sr-only">Argent Bank</h1>
+      </a>
+      <div>
+        <a className="main-nav-item" href="./sign-in.html">
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
