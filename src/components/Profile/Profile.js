ae0352f21713bae30d1b7e83e73e7c6583ae66e6
Mehdi Raheemun <mraheemu@student.42.fr>
2023-10-11 17:00:54 +0200

    bouton basic pour editer info Profil

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,17 +1,72 @@
 import React from "react";
 import "./Profile.css";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getProfile, updateProfile } from "../../redux/actions/authActions";
 
 function Profile() {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+  const [isEditing, setIsEditing] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  useEffect(() => {
+    if (!user) {
+      dispatch(getProfile());
+    } else {
+      setFirstName(user.firstName);
+      setLastName(user.lastName);
+    }
+  }, [user, dispatch]);
+
+  const handleEditClick = () => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async () => {
+    await dispatch(updateProfile({ firstName, lastName }));
+    setIsEditing(false);
+  };
+
+  const handleCancel = () => {
+    setIsEditing(false);
+    setFirstName(user.firstName);
+    setLastName(user.lastName);
+  };
+
   return (
     <main className="main bg-dark">
       <div className="header">
         <h1>
           Welcome back
           <br />
-          Tony Jarvis!
+          {user ? `${user.firstName} ${user.lastName}` : "Loading..."}!
         </h1>
-        <button className="edit-button">Edit Name</button>
+        {!isEditing && (
+          <button className="edit-button" onClick={handleEditClick}>
+            Edit Name
+          </button>
+        )}
       </div>
+      {isEditing && (
+        <div>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            placeholder="First Name"
+          />
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            placeholder="Last Name"
+          />
+          <button onClick={handleSave}>Save</button>
+          <button onClick={handleCancel}>Cancel</button>
+        </div>
+      )}
       <h2 className="sr-only">Accounts</h2>
       <section className="account">
         <div className="account-content-wrapper">
